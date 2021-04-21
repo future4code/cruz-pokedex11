089016b901d21e670eac7a84a893a662780379ee
Isabelle Frederico Travasso <isa.frederico@gmail.com>
2021-04-21 16:57:54 -0300

    Arrumando mudança de pagina com paginacao

--- pokedex/src/Components/CardPokedex.js
+++ pokedex/src/Components/CardPokedex.js
@@ -3,21 +3,10 @@ import GlobalStateContext from '../Global/GlobalStateContext'
 
 export const CardPokedex = (props) => {
 
-    const { states, requests } = useContext(GlobalStateContext)
+    const { requests } = useContext(GlobalStateContext)
+
 
     return (
-        // states.pokedex && states.pokedex.map((poke) => {
-        //     <div key={poke.id}>
-        //         <img src={poke.sprites.versions['generation-v']['black-white'].animated.front_default} alt={poke.name} />
-        //         <h2>{poke.name}</h2>
-        //         <p>{poke.type}</p>
-        //         <div>
-        //             <button onClick={() => requests.removePokedex(poke)}>Remover</button>
-        //             <button onClick={() => requests.pokemonDetails(poke)}>Ver detalhes</button>
-        //         </div>
-        //     </div>
-        // })
-
             <div>
                 <img src={props.image} alt={props.name} />
                 <h2>{props.name}</h2>
--- pokedex/src/Pages/Pokedex.js
+++ pokedex/src/Pages/Pokedex.js
@@ -5,46 +5,27 @@ import GlobalStateContext from '../Global/GlobalStateContext'
 export function Pokedex() {
     const { states, requests } = useContext(GlobalStateContext)
 
-    const renderPage = () => {
-
-        {
-            states.pokedex && states.pokedex.map((poke) => {
-                <div>
-                    <img src={poke.image} alt={poke.name} />
-                    <h2>{poke.name}</h2>
-                    <p>{poke.type}</p>
-                    <div>
-                        <button onClick={() => requests.removePokedex(poke.poke)}>Remover</button>
-                        <button onClick={() => requests.pokemonDetails(poke.poke)}>Ver detalhes</button>
-                    </div>
-                </div>
-            })
-        }
-
-
-    }
-
     return (
         <div>
-            {renderPage}
             {states.pokedex.length === 0 ? <p>Carregando...</p> : (
                 states.pokedex && states.pokedex.map((poke) => {
-                    // <CardPokedex
-                    //     key={poke.id}
-                    //     image={poke.sprites.versions['generation-v']['black-white'].animated.front_default}
-                    //     name={poke.name}
-                    //     type={poke.types[0].type.name}
-                    //     poke={poke}
-                    // />
-                    <div>
-                        <img src={poke.image} alt={poke.name} />
-                        <h2>{poke.name}</h2>
-                        <p>{poke.type}</p>
-                        <div>
-                            <button onClick={() => requests.removePokedex(poke.poke)}>Remover</button>
-                            <button onClick={() => requests.pokemonDetails(poke.poke)}>Ver detalhes</button>
-                        </div>
-                    </div>
+                    <CardPokedex
+                        key={poke.name}
+                        image={poke.sprites.versions['generation-v']['black-white'].animated.front_default}
+                        name={poke.name}
+                        type={poke.types[0].type.name}
+                        poke={poke}
+                    />
+                    // <div>
+                    //     <img src={poke.sprites.versions['generation-v']['black-white'].animated.front_default} alt={poke.name} />
+                    //     <h2>{poke.name}</h2>
+                    //     <p>{poke.types[0] && <p>{poke.types[0].type.name}</p>}</p>
+                    //     <div>
+                    //         <button onClick={() => requests.addPokedex(poke)}>Adicionar</button>
+                    //         <button onClick={() => requests.pokemonDetails(poke)}>Ver detalhes</button>
+                    //     </div>
+                    // </div>
+
 
                 })
             )
