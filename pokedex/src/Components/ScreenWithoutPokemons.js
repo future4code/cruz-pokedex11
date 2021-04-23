c3761752ebb4286e70b730278de73c15d6a0123d
Adryane Fernandes <edryfernandes@gmail.com>
2021-04-23 19:34:26 -0300

    Tela de erro

--- pokedex/src/Components/ScreenWithoutPokemons.js
+++ pokedex/src/Components/ScreenWithoutPokemons.js
@@ -1,35 +1,10 @@
 import pokeballs from '../img/pokeballs.png'
-import styled from 'styled-components'
-
-const Container = styled.main`
-  width: 90vw;
-  height: 78vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-const Image = styled.img `
-  width: 20%;
-`
-
-const Messages = styled.p `
-  font-weight: 700;
-  font-size: 2rem;
-  margin: 0;
-`
-const Title = styled(Messages) `
-  color: red;
-`
-const SubTitle = styled(Messages) `
-  font-size: 1rem;
-`
+import {Container, Image, Title, SubTitle} from '../Style/components/SrcreenWithoutPokemonsStyles'
 
 function ScreenWithoutPokemons() {
   return <Container>
     <Image src={pokeballs} alt={'pikachu em cima da pokebola'} />
-    <Title>Ops! Pokédex vazia</Title>
+    <Title>Pokédex vazia</Title>
     <SubTitle>Pokemons tem que pegar!</SubTitle>
   </Container>
 }
